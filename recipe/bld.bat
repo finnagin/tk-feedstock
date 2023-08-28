fd399c1a5709b6e979107b6cb969d9af9d25ba3a
Finn Womack <flan313@gmail.com>
2023-08-28 13:18:58 -0700

    print cwd and contents

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -27,7 +27,9 @@ if "%ARCH%"=="32" (
 )
 
 pushd tcl%PKG_VERSION%\win
+echo %cd%
 nmake nmakehlp.exe
+dir
 nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% release
 nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
 if %ERRORLEVEL% GTR 0 exit 1
