2296d8072db545004ecdf81c56eabdcc32b72578
Finn Womack <flan313@gmail.com>
2023-09-06 12:54:42 -0700

    comment out PATH change

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -31,12 +31,12 @@ nmake nmakehlp.exe INSTALLDIR=%LIBRARY_PREFIX%
 set PATH=%PATH%;%SRC_DIR%\tcl%PKG_VERSION%\win
 if %ARCH%=="ARM64" (
    echo "^^^^^^^^^^^^^^^^^^^^^^^^ build native tcl ^^^^^^^^^^^^^^^^^^^^^^^^"
-   where nmake
+   :: where nmake
    mkdir %SRC_DIR%\tclnative
-   nmake nmakehlp.exe INSTALLDIR=%SRC_DIR%\tclnative
-   set OLD_PATH=%PATH%
-   set PATH=%PATH%;%SRC_DIR%\tclnative
-   echo %OLD_PATH%
+   :: nmake nmakehlp.exe INSTALLDIR=%SRC_DIR%\tclnative
+   :: set OLD_PATH=%PATH%
+   :: set PATH=%PATH%;%SRC_DIR%\tclnative
+   :: echo %OLD_PATH%
    nmake -f makefile.vc INSTALLDIR=%SRC_DIR%\tclnative MACHINE="AMD64" ARCH="AMD64" release
    nmake -f makefile.vc INSTALLDIR=%SRC_DIR%\tclnative MACHINE="AMD64" ARCH="AMD64" install
    set TCLSH_NATIVE=%SRC_DIR%\tclnative
