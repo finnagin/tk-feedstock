ae5453956e971e9e2f5318ea7f10ce885bf793fc
Finn Womack <flan313@gmail.com>
2023-09-06 01:33:15 -0700

    fix conditional

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -29,7 +29,7 @@ if "%ARCH%"=="32" (
 pushd tcl%PKG_VERSION%\win
 nmake nmakehlp.exe INSTALLDIR=%LIBRARY_PREFIX%
 set PATH=%PATH%;%SRC_DIR%\tcl%PKG_VERSION%\win
-if "%ARCH%"=="arm64" (
+if %ARCH%=="ARM64" (
    nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX%\native MACHINE="AMD64" release
    nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX%\native MACHINE="AMD64" install
    set TCLSH_NATIVE=%LIBRARY_PREFIX%\native
