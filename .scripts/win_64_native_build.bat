4502fa61022d1d3a42218c91760f485d9203196a
Finn Womack <flan313@gmail.com>
2023-09-25 02:11:43 -0700

    Create win_64_native_build.bat

---
+++ .scripts/win_64_native_build.bat
@@ -0,0 +1,16 @@
+set "PATH=C:\WINDOWS\system32;C:\WINDOWS;%~1"
+set "LIBRARY_PREFIX=%~1\win-amd64"
+
+set MACHINE="AMD64"
+set ARCH="AMD64"
+
+if %2=="14" (
+  echo "Switching SDK versions"
+  call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64 10.0.15063.0
+)
+
+pushd %~1
+nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% release
+nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
+if %ERRORLEVEL% GTR 0 exit 1
+popd
