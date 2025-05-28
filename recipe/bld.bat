63a1ae0496dee18220da03254f406ad7139a9380
Isuru Fernando <isuruf@gmail.com>
2025-05-27 20:22:24 -0500

    set build machine

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,3 @@
-if "%target_platform%"=="win-32" (
-   set MACHINE="IX86"
-)
 if "%target_platform%"=="win-64" (
   set MACHINE="AMD64"
 )
@@ -8,12 +5,19 @@ if "%target_platform%"=="win-arm64" (
   set MACHINE="ARM64"
 )
 
+if "%build_platform%"=="win-64" (
+  set BUILD_MACHINE="AMD64"
+)
+if "%build_platform%"=="win-arm64" (
+  set BUILD_MACHINE="ARM64"
+)
+
 if NOT "%target_platform%"=="%build_platform%" (
   set "TCLSH_NATIVE=TCLSH_NATIVE=%BUILD_PREFIX%\Library\bin\tclsh86.exe"
 )
 
 pushd tcl%PKG_VERSION%\win
-nmake nmakehlp.exe
+nmake nmakehlp.exe MACHINE=%BUILD_MACHINE%
 nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% %TCLSH_NATIVE% MACHINE=%MACHINE% release
 nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% %TCLSH_NATIVE% MACHINE=%MACHINE% install
 if %ERRORLEVEL% GTR 0 exit 1
@@ -22,7 +26,7 @@ popd
 :: Tk build
 
 pushd tk%PKG_VERSION%\win
-nmake nmakehlp.exe
+nmake nmakehlp.exe MACHINE=%BUILD_MACHINE%
 nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% %TCLSH_NATIVE% MACHINE=%MACHINE% TCLDIR=..\..\tcl%PKG_VERSION% release
 nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% %TCLSH_NATIVE% MACHINE=%MACHINE% TCLDIR=..\..\tcl%PKG_VERSION% install
 if %ERRORLEVEL% GTR 0 exit 1
